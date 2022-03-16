32678aaa52a763bc33f7ecadf1c12dda7c98716a
Abrar Shahriar <abetrack3@yahoo.com>
2022-03-17 02:47:52 +0600

    convert from any base to decimal and vice versa

--- NumberBaseConverter.java
+++ NumberBaseConverter.java
@@ -2,33 +2,71 @@ import java.util.Scanner;
 
 public class NumberBaseConverter {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         // write your code here
+
         Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter a number in decimal system: ");
-        int num = sc.nextInt();
-        System.out.print("Enter target base: ");
-        int base = sc.nextInt();
-        
-        String result = "";
-        while(num != 0) {
-            result = getSymbol(num % base) + result;
-            num /= base;
+
+        System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
+        String mode;
+        while (!(mode = sc.next()).equals("/exit")) {
+            switch (mode) {
+                case "/from" : {
+                    System.out.println("Enter a number in decimal system: ");
+                    int decimal = Integer.parseInt(sc.next());
+                    System.out.println("Enter the target base: ");
+                    int base = Integer.parseInt(sc.next());
+                    System.out.println("Conversion result: " + convertFromDecimal(decimal, base) + "\n");
+                    break;
+                }
+                case "/to" : {
+                    System.out.println("Enter source number: ");
+                    String source = sc.next();
+                    System.out.println("Enter source base: ");
+                    int base = Integer.parseInt(sc.next());
+                    System.out.println("Conversion to decimal result: " + convertToDecimal(source, base) + "\n");
+                    break;
+                }
+            }
+            System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
         }
-        System.out.println("Conversion result: " + result);
     }
-    
-    public static String getSymbol(int val) {
-    	
-    	switch(val) {
-    		case 10: return "A";
-    		case 11: return "B";
-    		case 12: return "C";
-    		case 13: return "D";
-    		case 14: return "E";
-    		case 15: return "F";
-    		default: return "" + val;
-    	}
+
+    public static int convertToDecimal(String source, int sourceBase) {
+        int ans = 0;
+        int amp = 1;
+        for (int i = source.length(); i --> 0;) {
+            ans += getLiteralValue(source.charAt(i)) * amp;
+            amp *= sourceBase;
+        }
+        return ans;
+    }
+
+    private static String convertFromDecimal(int dec, int targetBase) {
+        StringBuilder result = new StringBuilder();
+        while (dec != 0) {
+            result.insert(0, getLiteral(dec % targetBase));
+            dec /= targetBase;
+        }
+        return result.toString();
+    }
+
+    private static int getLiteralValue (char c) {
+        if ('0' <= c && c <= '9') {
+            return c - 48;
+        } else if ('A' <= c && c <= 'Z') {
+            return c - 65 + 10;
+        } else {
+            return c - 97 +10;
+        }
+    }
+
+    private static String getLiteral(int a) {
+        if (a < 10) {
+            return String.valueOf(a);
+        } else {
+            char c = (char) (65 + a - 10);
+            return String.valueOf(c);
+        }
     }
 }
