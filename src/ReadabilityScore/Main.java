e1fe6b76545210ac666f47c32fa22ae680eb6753
Abrar Shahriar <abetrack3@yahoo.com>
2022-05-03 05:54:54 +0600

    Completed Readability Score: Stage 1

---
+++ src/ReadabilityScore/Main.java
@@ -0,0 +1,12 @@
+package ReadabilityScore;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String text = new Scanner(System.in).nextLine();
+        System.out.println(text.length() <= 100 ? "EASY" : "HARD");
+        
+    }
+}
