faae48f24c268399e253b72c46e1f9eb8f7736fc
Abrar Shahriar <abetrack3@yahoo.com>
2022-05-03 05:57:02 +0600

    Completed Readability Score: Stage 2

--- src/ReadabilityScore/Main.java
+++ src/ReadabilityScore/Main.java
@@ -6,7 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
         String text = new Scanner(System.in).nextLine();
-        System.out.println(text.length() <= 100 ? "EASY" : "HARD");
-        
+        String[] sentences = text.split("[!?.]\\s?");
+        int totalWordCount = 0;
+        for (String eachSentence : sentences) {
+            int wordCount = eachSentence.split("\\s").length;
+            totalWordCount += wordCount;
+        }
+        double averageWordCountPerSentence = (double) totalWordCount / sentences.length;
+        System.out.println(averageWordCountPerSentence <= 10.0 ? "EASY" : "HARD");
     }
 }
