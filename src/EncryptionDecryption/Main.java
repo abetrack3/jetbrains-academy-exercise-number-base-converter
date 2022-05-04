640411bdc4e55e7e6ab8fe4a3c94ba5a30c2093f
Abrar Shahriar <abetrack3@yahoo.com>
2022-05-04 18:56:24 +0600

    Encryption-Decryption: Stage-4

--- src/EncryptionDecryption/Main.java
+++ src/EncryptionDecryption/Main.java
@@ -1,23 +1,32 @@
 package EncryptionDecryption;
 
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String mode = scanner.nextLine();
-        String message = scanner.nextLine();
-        int key = Integer.parseInt(scanner.nextLine());
-        System.out.println(cipherTextEncrypt(message, key, mode.equals("dec")));
+    public static void main(String... args) {
+        Map<String, String> arguments = new HashMap<>() {{
+            put("mode", "enc");
+            put("key", "0");
+            put("data", "");
+        }};
+        for (int i = 0; i < args.length - 1; i += 2) {
+            arguments.put(args[i].replace("-", ""), args[i + 1]);
+        }
+        System.out.println(cipherTextEncrypt(
+                arguments.get("data"),
+                Integer.parseInt(arguments.get("key")),
+                arguments.get("mode").equals("enc")
+        ));
     }
 
-    public static String cipherTextEncrypt(String text, int key, boolean decrypt) {
+    public static String cipherTextEncrypt(String text, int key, boolean encrypt) {
         return text
                 .chars()
-                .map(e -> encryptChar(e, decrypt ? -key : key))
-                .mapToObj(e -> String.valueOf((char) e))
+                .map(e -> encryptChar(e, encrypt ? key : -key))
+                .mapToObj(Character::toString)
                 .collect(Collectors.joining());
     }
 
